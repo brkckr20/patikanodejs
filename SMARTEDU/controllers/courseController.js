450df676e250fb9e4f77a8555529f7d49fee1ee3
Burak Çakır <burak2043@gmail.com>
2022-07-13 10:15:21 +0300

    search alani eklendi

--- SMARTEDU/controllers/courseController.js
+++ SMARTEDU/controllers/courseController.js
@@ -29,13 +29,30 @@ exports.getAllCourses = async (req, res) => {
     try {
 
         const categorySlug = req.query.categories;
+
+        const query = req.query.search;
+
         const category = await Category.findOne({ slug: categorySlug });
         let filter = {};
         if (categorySlug) {
             filter = { category: category._id };
         }
 
-        const courses = await Course.find(filter).sort("-createdAt");
+        if (query) {
+            filter = { name: query }
+        }
+
+        if (!query && !categorySlug) {
+            filter.name = "";
+            filter.category = null
+        }
+
+        const courses = await Course.find({
+            $or: [
+                { name: { $regex: ".*" + filter.name + ".*", $options: "i" } },
+                { category: filter.category }
+            ]
+        }).sort("-createdAt").populate("user");
         const categories = await Category.find();
         res.status(200).render("courses", {
             courses: courses,
@@ -56,10 +73,13 @@ exports.getCourse = async (req, res) => {
     try {
         const user = await User.findById(req.session.userID)
         const course = await Course.findOne({ slug: req.params.slug }).populate("user");
+        const categories = await Category.find();
+
         res.status(200).render("course", {
             course: course,
             pageName: "course",
-            user: user
+            user: user,
+            categories: categories
         })
 
     } catch (error) {
